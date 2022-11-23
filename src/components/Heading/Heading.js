4edff7c9d8def2be5ac2f1b29ecddbad0932d3d4
Utkarsh Singh <83408377+utkarsh7370@users.noreply.github.com>
2022-11-23 21:00:41 +0530

    Create Heading.js

---
+++ src/components/Heading/Heading.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Heading.css";
+
+function Heading(props) {
+  return (
+    <>
+      <div class="heading">
+        <h1 class="bg-heading">{props.title}</h1>
+        <h1 class="front-heading">{props.title}</h1>
+      </div>
+    </>
+  );
+}
+
+export default Heading;

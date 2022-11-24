270b3b52c3e0d405d43918002d55f778af8b4454
Nitu Kumari <78076309+U220053@users.noreply.github.com>
2022-11-25 02:05:22 +0530

    venue

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import HeroSection from "./components/HeroSection";
 import Teachers from "./components/Teachers/Teachers";
 
 import "./App.css";
+import Venue from "./components/Venue";
 
 function App() {
   return (
     <>
       <HeroSection />
       <Teachers />
+      <Venue/>
       <Footer />
     </>
   );

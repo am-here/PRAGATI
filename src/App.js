dca002db0ee87cec4e5d8531b7002f8d8b092252
Utkarsh Singh <83408377+utkarsh7370@users.noreply.github.com>
2022-11-25 11:32:16 +0530

    navbaar1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import Footer from "./components/Footer";
-import HeroSection from "./components/HeroSection";
+// import HeroSection from "./components/HeroSection";
 import Teachers from "./components/Teachers/Teachers";
-import Day from "./Scheduler/Day";
-
+// import Day from "./Scheduler/Day";
+// import HeroSections from "./components/Home/HeroSections";
 import "./App.css";
+// import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
-      <Day />
-      <HeroSection />
+      {/* <Navbar /> */}
+      {/* <HeroSections /> */}
+      {/* <Day /> */}
+      {/* <HeroSection /> */}
       <Teachers />
       <Footer />
     </>

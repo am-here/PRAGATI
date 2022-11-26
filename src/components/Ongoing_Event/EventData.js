a49fb36b473b88a13e5fac1fbfdc85a29defcf76
Utkarsh Singh <83408377+utkarsh7370@users.noreply.github.com>
2022-11-26 23:07:50 +0530

    Event Commit

--- src/components/Ongoing_Event/EventData.js
+++ src/components/Ongoing_Event/EventData.js
@@ -4,31 +4,6 @@ const EventData = [
     description:
       "Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.",
   },
-  {
-    image: "logo_comp.png",
-    description:
-      "Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.",
-  },
-  {
-    image: "logo_comp.png",
-    description:
-      "Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.",
-  },
-  {
-    image: "logo_comp.png",
-    description:
-      "Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.",
-  },
-  {
-    image: "logo_comp.png",
-    description:
-      "Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.",
-  },
-  {
-    image: "logo_comp.png",
-    description:
-      "Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.",
-  },
 ];
 
 export default EventData;

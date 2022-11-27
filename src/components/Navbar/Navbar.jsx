886156e1a35bfe99e0e59eba419c2dc506e2267e
Ishika Ghosh <idofishika321@gmail.com>
2022-11-27 18:24:25 +0530

    navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,9 +3,11 @@ import { HashLink } from "react-router-hash-link";
 import { Link } from "react-router-dom";
 
 import "./Navbar.css";
+import "./Hambarger.css";
 
 function Navbar() {
   const [show, setShow] = useState(false);
+  const [hide, setHide] = useState(true);
   useEffect(() => {
     window.addEventListener("scroll", () => {
       if (window.scrollY > 80) {
@@ -18,36 +20,55 @@ function Navbar() {
       window.removeEventListener("scroll", window);
     };
   }, []);
+  const handleClick = () => {
+    setHide(!hide);
+  };
   return (
     <div className={`nav ${show && "nav_black"}`}>
       <div className="nav-container">
-        <Link to="/" className="logo">
-          {/* <img src={logo} alt="LOGO" className="nav-logo" /> */}
-          Pragati
-        </Link>
+        <div className="logo-menu">
+          <Link to="/" className="logo">
+            {/* <img src={logo} alt="LOGO" className="nav-logo" /> */}
+            Pragati
+          </Link>
+          <div className="menu-btn" onClick={handleClick}>
+            <button
+              className={
+                hide
+                  ? "hamburger hamburger--collapse"
+                  : "hamburger hamburger--collapse is-active"
+              }
+              type="button"
+            >
+              <span className="hamburger-box">
+                <span className="hamburger-inner"></span>
+              </span>
+            </button>
+          </div>
+        </div>
 
-        <div className="items">
-          <span className="nav-items">
+        <div className={hide ? "items hide" : "items"}>
+          <span className="nav-items" onClick={handleClick}>
             <HashLink to="/#home" className="nav-links">
               Home
             </HashLink>
           </span>
-          <span className="nav-items">
+          <span className="nav-items" onClick={handleClick}>
             <HashLink to="/#events" className="nav-links">
               Events
             </HashLink>
           </span>
-          <span className="nav-items">
+          <span className="nav-items" onClick={handleClick}>
             <HashLink to="/schedule" className="nav-links">
               Schedule
             </HashLink>
           </span>
-          <span className="nav-items">
+          <span className="nav-items" onClick={handleClick}>
             <HashLink to="/#venue" className="nav-links">
               Venue
             </HashLink>
           </span>
-          <span className="nav-items">
+          <span className="nav-items" onClick={handleClick}>
             <HashLink to="/contact" className="nav-links">
               Contacts
             </HashLink>

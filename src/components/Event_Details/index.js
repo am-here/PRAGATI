89d7b5a7cc9313cb17d2b2c548a2646d768ea7de
am-here <harshbaid.ind@gmail.com>
2022-12-16 23:10:36 +0530

    forced commit

---
+++ src/components/Event_Details/index.js
@@ -0,0 +1,15 @@
+import "./details.css";
+import Heading from "../Heading";
+
+function Event_Detail() {
+    return (
+        <>
+            <div id="event">
+                <Heading title="DETAILS" />
+                <img src="" alt="" className="qr_code" />
+            </div>
+        </>
+    );
+}
+
+export default Event_Detail;

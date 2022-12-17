2a60f14fbcfee5e9909ca3e6bb0f85f57940ddc5
am-here <harshbaid.ind@gmail.com>
2022-12-17 19:17:36 +0530

    conflicts handled and modified

--- src/components/Event_Details/Data.js
+++ src/components/Event_Details/Data.js
@@ -21,8 +21,7 @@ const eventDetails = [
         { id: 17, name: "Ishika Sen ECE", phone: "6289649452", bio: "ECE" },
         { id: 18, name: "Shailee Sharma", phone: "9123709838", bio: "ECE" },
       ],
-    } 
-    ,
+    },
   },
   {
     id: 2,
@@ -33,34 +32,30 @@ const eventDetails = [
     size: "2 members",
     googlr_Form: "https://forms.gle/14k7Hzp8UC1UCJ839",
     rules: "https://meet.google.com/zua-gmya-vyf",
-    contacts:
-    {
-      incharge: [{ id: 21, name: "Ms. Nabamita Majumder"
-      
-       }],
-      incharge: [{ id: 22, name: "Mr. Basudev Ghatak" }],
-    
+    contacts: {
+      incharge: [
+        { id: 21, name: "Ms. Nabamita Majumder" },
+        { id: 22, name: "Mr. Basudev Ghatak" },
+      ],
+
       SubCommittee: [
         { id: 23, name: " Ms. Sasmita S Choudhury" },
-        { id: 24, name: " Mr. Kailash Chandra Mahatab"
-         },
+        { id: 24, name: " Mr. Kailash Chandra Mahatab" },
         { id: 25, name: " Ms. Aditi Bandyopadhyay" },
       ],
-     
-     
 
-      
-     
-     
-
-       StudentMembers: [
+      StudentMembers: [
         { id: 26, name: "  Debarshi Roy", phone: "9002189996", bio: "IT" },
-     
         { id: 27, name: " Arpan Saha Hazra", phone: "7003988236", bio: "ME" },
-        { id: 28, name: " Manjeet Kumar Shaw", phone: "7003083035", bio: "CSE" },
+        {
+          id: 28,
+          name: " Manjeet Kumar Shaw",
+          phone: "7003083035",
+          bio: "CSE",
+        },
       ],
     },
-  }, 
+  },
 
   {
     id: 3,
@@ -68,25 +63,20 @@ const eventDetails = [
     date: "5th January 2023",
     amtin: "FREE",
     amtout: "FREE",
-      
+
     size: "2 members",
     googlr_Form: "https://forms.gle/14k7Hzp8UC1UCJ839",
     rules: "https://meet.google.com/zua-gmya-vyf",
-    contacts:  {
-      incharge: [{ id: 31, name: "Mr. Puspen Lahiri"
-      
-       }],
-      incharge: [{ id: 32, name: "Mr. Basudev Ghatak" }],
-    
+    contacts: {
+      incharge: [{ id: 31, name: "Mr. Puspen Lahiri" },{ id: 32, name: "Mr. Basudev Ghatak" }],
       SubCommittee: [
         { id: 33, name: "  Mr. Prasenjit Das" },
-        { id: 34, name: " Ms. Moumita Goswami"
-         },
+        { id: 34, name: " Ms. Moumita Goswami" },
         { id: 35, name: " Mr. Rajib Chatterjee" },
       ],
-     StudentMembers: [
+      StudentMembers: [
         { id: 36, name: "   Baishali Ghosh", phone: "9330184233", bio: "CSE" },
-     
+
         { id: 37, name: "  Aindrila Dutta", phone: "7003647277", bio: "CSE" },
       ],
     },
@@ -96,38 +86,31 @@ const eventDetails = [
     name: "AutoCad/Catia",
     date: "5th January 2023",
     amtin: "₹50",
-    amtout:"₹100",
+    amtout: "₹100",
     size: "2 members",
     googlr_Form: "https://forms.gle/14k7Hzp8UC1UCJ839",
     rules: "https://meet.google.com/zua-gmya-vyf",
-    contacts:
-    {   incharge: [{ id: 41, name: "Mr. Swagata Shanigrihi,"
-      
-  }],
- incharge: [{ id: 42, name: "Mr. Basudev Ghatak" }],
+    contacts: {
+      incharge: [{ id: 41, name: "Mr. Swagata Shanigrihi," },{ id: 42, name: "Mr. Basudev Ghatak" }],
 
- 
+      SubCommittee: [
+        { id: 43, name: "   Mr. Swarup Ch. Giri" },
+        { id: 44, name: " Mr. Shyam Sundar Dutta" },
+        { id: 45, name: " Mr. Ashim Chakraborty" },
+      ],
+      StudentMembers: [
+        { id: 46, name: "  Swati Gupta", phone: "9748694311", bio: "ME" },
 
- SubCommittee: [
-   { id: 43, name: "   Mr. Swarup Ch. Giri" },
-   { id: 44, name: " Mr. Shyam Sundar Dutta"
+        { id: 47, name: " Souhardik Kundu", phone: "8101284655", bio: "AUE" },
+      ],
     },
-   { id: 45, name: " Mr. Ashim Chakraborty" },
- ],
-StudentMembers: [
-   { id: 46, name: "  Swati Gupta", phone: "9748694311", bio: "ME" },
-
-   { id: 47, name: " Souhardik Kundu", phone: "8101284655", bio: "AUE" },
-   
- ],
-},
-},
+  },
   {
     id: 5,
     name: "Innovative Idea Contest",
     date: "5th January 2023",
     amtin: "₹100",
-    amtout:"₹200",
+    amtout: "₹200",
     size: "4 members",
     googlr_Form: "https://forms.gle/co64X9ixbpViitgV6",
     rules: "https://meet.google.com/zua-gmya-vyf",
@@ -151,7 +134,7 @@ StudentMembers: [
     name: "Innovative Model Contest",
     date: "2nd January 2023",
     amtin: "₹100",
-    amtout:"₹200",
+    amtout: "₹200",
     size: "4 members",
     googlr_Form: "https://forms.gle/N8Yp4VkrRMZyGjpT6",
     rules: "https://meet.google.com/zua-gmya-vyf",
@@ -174,9 +157,9 @@ StudentMembers: [
     id: 7,
     name: "Technical Poster",
     date: "5th January 2023",
-    amtin:"₹100",
+    amtin: "₹100",
     amtout: "₹200 ",
-     size: "3 members",
+    size: "3 members",
     googlr_Form: "https://forms.gle/v2yqUHwkHzQmNhdn6",
     rules: "https://meet.google.com/zua-gmya-vyf",
     contacts: {
@@ -196,35 +179,24 @@ StudentMembers: [
   },
   {
     id: 8,
-  name: "Math Mania",
+    name: "Math Mania",
     date: "5th January 2023",
     amtin: "₹50",
-    amtout:"₹100",
+    amtout: "₹100",
     size: "2 members",
     googlr_Form: "https://forms.gle/14k7Hzp8UC1UCJ839",
     rules: "https://meet.google.com/zua-gmya-vyf",
-    contacts: 
-    {   incharge: [{ id: 41, name: "Mr. Milan Chakraborty,"
-      
-  }],
- incharge: [{ id: 42, name: " Mr. Sourav Ghorai"
- }],
+    contacts: {
+      incharge: [{ id: 41, name: "Mr. Milan Chakraborty," },{ id: 42, name: "Mr. Sourav Ghorai" }],
 
+      SubCommittee: [{ id: 43, name: " Dr. Abhijit Pramanik" }],
+      StudentMembers: [
+        { id: 46, name: "  Romit Ghosh", phone: "6297525110", bio: "EE" },
 
- 
-
- SubCommittee: [
-   { id: 43, name: " Dr. Abhijit Pramanik" },
-  
- ],
-StudentMembers: [
-   { id: 46, name: "  Romit Ghosh", phone: "6297525110", bio: "EE" },
-
-   { id: 47, name: " Akansha Tripathi", phone: "6290839047", bio: "IT" },
-   
- ],
-},
-},
+        { id: 47, name: " Akansha Tripathi", phone: "6290839047", bio: "IT" },
+      ],
+    },
+  },
   {
     id: 9,
     name: "Quiz-o-mania",
@@ -253,7 +225,7 @@ StudentMembers: [
     name: "Treasure Hunt",
     date: "5th January 2023",
     amtin: "₹50",
-    amtout:"₹50",
+    amtout: "₹50",
     size: "1 to 3",
     googlr_Form: "https://forms.gle/14k7Hzp8UC1UCJ839",
     rules: "https://meet.google.com/zua-gmya-vyf",
@@ -277,29 +249,19 @@ StudentMembers: [
     name: "NFS Most Wanted",
     date: "5th January 2023",
     amtin: "₹50",
-    amtout:"₹100",
+    amtout: "₹100",
     size: "1 member",
     googlr_Form: "https://forms.gle/14k7Hzp8UC1UCJ839",
     rules: "https://meet.google.com/zua-gmya-vyf",
-    contacts: 
-    {
-      incharge: [{ id: 111, name: "Ms Jayanti Mahata"
-      
-       }],
-      incharge: [{ id: 112, name: "Mr. Basudev Ghatak" }],
-    
-      SubCommittee: [
-        { id: 113, name: " Mr. Amlan Karmakar" },
-        
-      ],
-     
-      
+    contacts: {
+      incharge: [{ id: 111, name: "Ms Jayanti Mahata" },{ id: 112, name: "Mr. Basudev Ghatak" }],
 
-       StudentMembers: [
+      SubCommittee: [{ id: 113, name: " Mr. Amlan Karmakar" }],
+
+      StudentMembers: [
         { id: 114, name: " Aditya Lal Shah ", phone: "8981195810", bio: "AUE" },
-     
+
         { id: 115, name: " Mrinmoy Sasmal", phone: "7890175823", bio: "ME" },
-       
       ],
     },
   },
@@ -308,30 +270,29 @@ StudentMembers: [
     name: "FIFA",
     date: "5th January 2023",
     amtin: "₹50",
-    amtout:"₹100",
+    amtout: "₹100",
     size: "1 member",
     googlr_Form: "https://forms.gle/14k7Hzp8UC1UCJ839",
     rules: "https://meet.google.com/zua-gmya-vyf",
-    contacts: 
-    {
-      incharge: [{ id: 121, name: "Mr. Santanu Maity"
-      
-      
-       }],
-      incharge: [{ id: 122, name: "Mr. Basudev Ghatak" }],
-    
-      SubCommittee: [
-        { id: 123, name: " Mr. Sandip Chakraborty" },
-        
-      ],
-      
-   
-      
+    contacts: {
+      incharge: [{ id: 121, name: "Mr. Santanu Maity" },{ id: 122, name: "Mr. Basudev Ghatak" }],
 
-       StudentMembers: [
-        { id: 124, name: "Archisman Chatterjee", phone: "7619460342", bio: " CSE" },
-     
-        { id: 125, name: "    Lytza Chatterjee", phone: "8240864191", bio: " AUEE" },
+      SubCommittee: [{ id: 123, name: " Mr. Sandip Chakraborty" }],
+
+      StudentMembers: [
+        {
+          id: 124,
+          name: "Archisman Chatterjee",
+          phone: "7619460342",
+          bio: " CSE",
+        },
+
+        {
+          id: 125,
+          name: "    Lytza Chatterjee",
+          phone: "8240864191",
+          bio: " AUEE",
+        },
       ],
     },
   },
@@ -345,32 +306,18 @@ StudentMembers: [
     googlr_Form: "https://forms.gle/14k7Hzp8UC1UCJ839",
     rules: "https://meet.google.com/zua-gmya-vyf",
     contacts: {
-
-      incharge: [{ id: 131, name: "Dr. Abhijit Dutta"
-      
-       }],
-      incharge: [{ id: 132, name: "Mr. Tapas Kumar Biswas" }],
-      incharge: [{ id: 133, name: "Mr. Nabankur Mandal" }],
+      incharge: [{ id: 131, name: "Dr. Abhijit Dutta" },{ id: 132, name: "Mr. Tapas Kumar Biswas" },{ id: 133, name: "Mr. Nabankur Mandal" }],
       SubCommittee: [
         { id: 134, name: " Mr. Bimal Makhal" },
         { id: 135, name: "Mr. Drupad Roy" },
         { id: 136, name: "Mr. Dhiman Bhattacharjee" },
         { id: 137, name: "Mr. Rajib Majhi" },
-        { id: 138, name: "Mr. Samrat Purkait "},
-        { id: 139, name: "Mr. Om Prakash"},
-        { id: 1310, name: "Mr. Arijit Ghosh"},
-        { id: 1311, name: "Mr. Joydeb Karmakar"},
-        { id: 1312, name: "Mr. Ashis Taru Roy"},
+        { id: 138, name: "Mr. Samrat Purkait " },
+        { id: 139, name: "Mr. Om Prakash" },
+        { id: 1310, name: "Mr. Arijit Ghosh" },
+        { id: 1311, name: "Mr. Joydeb Karmakar" },
+        { id: 1312, name: "Mr. Ashis Taru Roy" },
       ],
-     
-     
-      
-
-
-
-
-
-
 
       StudentMembers: [
         { id: 1313, name: "Nabamita Bakshi ", phone: "7044604970", bio: "ECE" },
@@ -383,32 +330,32 @@ StudentMembers: [
     name: "Robo War",
     date: "5th January 2023",
     amtin: "₹100",
-    amtout:"₹400",
+    amtout: "₹400",
     size: "2 to 4 members",
     googlr_Form: "https://forms.gle/14k7Hzp8UC1UCJ839",
     rules: "https://meet.google.com/zua-gmya-vyf",
-    contacts:
-    {
-      incharge: [{ id: 141, name: "Dr. Abhijit Dutta"
-      
-       }],
-      incharge: [{ id: 142, name: "Mr. Tapas Kumar Biswas" }],
-      incharge: [{ id: 143, name: "Mr. Nabankur Mandal" }],
+    contacts: {
+      incharge: [{ id: 141, name: "Dr. Abhijit Dutta" },{ id: 142, name: "Mr. Tapas Kumar Biswas" },{ id: 143, name: "Mr. Nabankur Mandal" }],
       SubCommittee: [
         { id: 144, name: " Mr. Bimal Makhal" },
         { id: 145, name: "Mr. Drupad Roy" },
         { id: 146, name: "Mr. Dhiman Bhattacharjee" },
         { id: 147, name: "Mr. Rajib Majhi" },
-        { id: 148, name: "Mr. Samrat Purkait "},
-        { id: 149, name: "Mr. Om Prakash"},
-        { id: 1410, name: "Mr. Arijit Ghosh"},
-        { id: 1411, name: "Mr. Joydeb Karmakar"},
-        { id: 1412, name: "Mr. Ashis Taru Roy"},
+        { id: 148, name: "Mr. Samrat Purkait " },
+        { id: 149, name: "Mr. Om Prakash" },
+        { id: 1410, name: "Mr. Arijit Ghosh" },
+        { id: 1411, name: "Mr. Joydeb Karmakar" },
+        { id: 1412, name: "Mr. Ashis Taru Roy" },
       ],
-       StudentMembers: [
+      StudentMembers: [
         { id: 1413, name: "Kaushik Shaw ", phone: "9875386612", bio: "EE" },
-        { id: 1414, name: " Shrayam Banerjee", phone: "9836482698", bio: "CSE" },
-        { id: 1415, name: " Akash Singh", phone:"9088801008" ,bio:"AUE"},
+        {
+          id: 1414,
+          name: " Shrayam Banerjee",
+          phone: "9836482698",
+          bio: "CSE",
+        },
+        { id: 1415, name: " Akash Singh", phone: "9088801008", bio: "AUE" },
       ],
     },
   },
@@ -417,36 +364,36 @@ StudentMembers: [
     name: "Chase the maze",
     date: "5th January 2023",
     amtin: "₹100",
-    amtout:"₹400",
+    amtout: "₹400",
     size: "2 members",
     googlr_Form: "https://forms.gle/14k7Hzp8UC1UCJ839",
     rules: "https://meet.google.com/zua-gmya-vyf",
 
-    contacts: 
-    {
-      incharge: [{ id: 151, name: "Dr. Abhijit Dutta"
-      
-  }],
- incharge: [{ id: 152, name: "Mr. Tapas Kumar Biswas" }],
- incharge: [{ id: 153, name: "Mr. Nabankur Mandal" }],
- SubCommittee: [
-   { id: 154, name: " Mr. Bimal Makhal" },
-   { id: 155, name: "Mr. Drupad Roy" },
-   { id: 156, name: "Mr. Dhiman Bhattacharjee" },
-   { id: 157, name: "Mr. Rajib Majhi" },
-   { id: 158, name: "Mr. Samrat Purkait "},
-   { id: 159, name: "Mr. Om Prakash"},
-   { id: 1510, name: "Mr. Arijit Ghosh"},
-   { id: 1511, name: "Mr. Joydeb Karmakar"},
-   { id: 1512, name: "Mr. Ashis Taru Roy"},
- ],
-  StudentMembers: [
-   { id: 1413, name: " Pratyay Ghosh", phone: "9051642232", bio: "AUE" },
-   { id: 1414, name: " Mohit Kumar Singh", phone: "9051802430", bio: "EE" },
-   
- ],
-},
-},
+    contacts: {
+      incharge: [{ id: 141, name: "Dr. Abhijit Dutta" },{ id: 142, name: "Mr. Tapas Kumar Biswas" },{ id: 143, name: "Mr. Nabankur Mandal" }],
+
+      SubCommittee: [
+        { id: 154, name: " Mr. Bimal Makhal" },
+        { id: 155, name: "Mr. Drupad Roy" },
+        { id: 156, name: "Mr. Dhiman Bhattacharjee" },
+        { id: 157, name: "Mr. Rajib Majhi" },
+        { id: 158, name: "Mr. Samrat Purkait " },
+        { id: 159, name: "Mr. Om Prakash" },
+        { id: 1510, name: "Mr. Arijit Ghosh" },
+        { id: 1511, name: "Mr. Joydeb Karmakar" },
+        { id: 1512, name: "Mr. Ashis Taru Roy" },
+      ],
+      StudentMembers: [
+        { id: 1413, name: " Pratyay Ghosh", phone: "9051642232", bio: "AUE" },
+        {
+          id: 1414,
+          name: " Mohit Kumar Singh",
+          phone: "9051802430",
+          bio: "EE",
+        },
+      ],
+    },
+  },
   {
     id: 16,
     name: "Line Follower",
@@ -457,32 +404,31 @@ StudentMembers: [
     googlr_Form: "https://forms.gle/14k7Hzp8UC1UCJ839",
     rules: "https://meet.google.com/zua-gmya-vyf",
 
-    contacts: 
-    {
-      incharge: [{ id: 161, name: "Dr. Abhijit Dutta"
-      
-       }],
-      incharge: [{ id: 162, name: "Mr. Tapas Kumar Biswas" }],
-      incharge: [{ id: 163, name: "Mr. Nabankur Mandal" }],
+    contacts: {
+      incharge: [{ id: 141, name: "Dr. Abhijit Dutta" },{ id: 142, name: "Mr. Tapas Kumar Biswas" },{ id: 143, name: "Mr. Nabankur Mandal" }],
+
       SubCommittee: [
         { id: 164, name: " Mr. Bimal Makhal" },
         { id: 165, name: "Mr. Drupad Roy" },
         { id: 166, name: "Mr. Dhiman Bhattacharjee" },
         { id: 167, name: "Mr. Rajib Majhi" },
-        { id: 168, name: "Mr. Samrat Purkait "},
-        { id: 169, name: "Mr. Om Prakash"},
-        { id: 1610, name: "Mr. Arijit Ghosh"},
-        { id: 1611, name: "Mr. Joydeb Karmakar"},
-        { id: 1612, name: "Mr. Ashis Taru Roy"},
+        { id: 168, name: "Mr. Samrat Purkait " },
+        { id: 169, name: "Mr. Om Prakash" },
+        { id: 1610, name: "Mr. Arijit Ghosh" },
+        { id: 1611, name: "Mr. Joydeb Karmakar" },
+        { id: 1612, name: "Mr. Ashis Taru Roy" },
       ],
-       StudentMembers: [
+      StudentMembers: [
         { id: 1613, name: "Ayan Palit", phone: "6295389311", bio: "EE" },
-        { id: 1614, name: " Nageswar Rao Enduga", phone: "8420559756", bio: "ME" },
-        { id: 1615, name: " Abhishek Kumar", phone:"6207902850" ,bio:"EE"},
+        {
+          id: 1614,
+          name: " Nageswar Rao Enduga",
+          phone: "8420559756",
+          bio: "ME",
+        },
+        { id: 1615, name: " Abhishek Kumar", phone: "6207902850", bio: "EE" },
       ],
     },
   },
 ];
 export default eventDetails;
-
-
--- src/components/Event_Details/EventContact.jsx
+++ src/components/Event_Details/EventContact.jsx
@@ -6,20 +6,20 @@ function EventContact({ contact }) {
     <>
       <div className="eventContact_container">
         <h1 className="committeeHead">
-          Event In-charge && Core committee members
+          CORE COMMITTEE MEMBERS
         </h1>
         <div className="carousel-container">
           {incharge.map((m, i) => (
             <Card key={i} details={m} />
           ))}
         </div>
-        <h1 className="committeeHead">Sub-committee Members</h1>
+        <h1 className="committeeHead">SUB-COMMITTEE MEMBERS</h1>
         <div className="carousel-container">
           {SubCommittee.map((m, i) => (
             <Card key={i} details={m} />
           ))}
         </div>
-        <h1 className="committeeHead">Student committee members</h1>
+        <h1 className="committeeHead">STUDENT COMMITTEE MEMBERS</h1>
         <div className="carousel-container">
           {StudentMembers.map((m, i) => (
             <Card key={i} details={m} />

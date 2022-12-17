f6014c97ea48faac0e22d92d65632fe07bc7f38b
Utkarsh Singh <83408377+utkarsh7370@users.noreply.github.com>
2022-12-17 22:16:23 +0530

    event details

--- src/components/Event_Details/Data.js
+++ src/components/Event_Details/Data.js
@@ -7,18 +7,19 @@ const eventDetails = [
     amtout: "₹50",
     size: "1 to 3 members",
     googlr_Form: "https://forms.gle/14k7Hzp8UC1UCJ839",
-    rules: "https://meet.google.com/zua-gmya-vyf",
+    rules:
+      "https://drive.google.com/file/d/10KZFcStsTGRRCnM5HJirJJChz7G8nzvJ/view?usp=share_link",
     contacts: {
       incharge: [{ id: 11, name: "Ms. Sushmita Dey" }],
       SubCommittee: [
         { id: 12, name: "Mr. Sekhar Rana" },
-        { id: 13, name: "Mr. Sekhar Rana" },
-        { id: 14, name: "Mr. Sekhar Rana" },
-        { id: 15, name: "Mr. Sekhar Rana" },
-        { id: 16, name: "Mr. Sekhar Rana" },
+        { id: 13, name: "Mr. Ajoy Ghosal" },
+        { id: 14, name: "Mr. Chandan Das" },
+        { id: 15, name: "Ms. Priyanka Nath" },
+        { id: 16, name: "Mr. Arijit Sen" },
       ],
       StudentMembers: [
-        { id: 17, name: "Ishika Sen ECE", phone: "6289649452", bio: "ECE" },
+        { id: 17, name: "Ishika Sen", phone: "6289649452", bio: "ECE" },
         { id: 18, name: "Shailee Sharma", phone: "9123709838", bio: "ECE" },
       ],
     },
@@ -31,7 +32,8 @@ const eventDetails = [
     amtout: "₹100",
     size: "2 members",
     googlr_Form: "https://forms.gle/14k7Hzp8UC1UCJ839",
-    rules: "https://meet.google.com/zua-gmya-vyf",
+    rules:
+      "https://drive.google.com/file/d/14jo8d6ts7BUQ325CjTny9PX_rwdg5Gok/view?usp=share_link",
     contacts: {
       incharge: [
         { id: 21, name: "Ms. Nabamita Majumder" },
@@ -66,9 +68,13 @@ const eventDetails = [
 
     size: "2 members",
     googlr_Form: "https://forms.gle/14k7Hzp8UC1UCJ839",
-    rules: "https://meet.google.com/zua-gmya-vyf",
+    rules:
+      "https://drive.google.com/file/d/16JbRipSaEunAaS-ZpsWezPy5hs9Jo1TX/view?usp=share_link",
     contacts: {
-      incharge: [{ id: 31, name: "Mr. Puspen Lahiri" },{ id: 32, name: "Mr. Basudev Ghatak" }],
+      incharge: [
+        { id: 31, name: "Mr. Puspen Lahiri" },
+        { id: 32, name: "Mr. Basudev Ghatak" },
+      ],
       SubCommittee: [
         { id: 33, name: "  Mr. Prasenjit Das" },
         { id: 34, name: " Ms. Moumita Goswami" },
@@ -89,9 +95,13 @@ const eventDetails = [
     amtout: "₹100",
     size: "2 members",
     googlr_Form: "https://forms.gle/14k7Hzp8UC1UCJ839",
-    rules: "https://meet.google.com/zua-gmya-vyf",
+    rules:
+      "https://drive.google.com/file/d/1ik51cXNPFOS-xtGUg_zz5tW4lkP1Zgi6/view?usp=share_link",
     contacts: {
-      incharge: [{ id: 41, name: "Mr. Swagata Shanigrihi," },{ id: 42, name: "Mr. Basudev Ghatak" }],
+      incharge: [
+        { id: 41, name: "Mr. Swagata Shanigrihi," },
+        { id: 42, name: "Mr. Basudev Ghatak" },
+      ],
 
       SubCommittee: [
         { id: 43, name: "   Mr. Swarup Ch. Giri" },
@@ -113,19 +123,21 @@ const eventDetails = [
     amtout: "₹200",
     size: "4 members",
     googlr_Form: "https://forms.gle/co64X9ixbpViitgV6",
-    rules: "https://meet.google.com/zua-gmya-vyf",
+    rules:
+      "https://drive.google.com/file/d/1X8FZCyvZt8wUaEnE5WUlt-OZVfy0huuq/view?usp=share_link",
     contacts: {
-      incharge: [{ id: 11, name: "Ms. Sushmita Dey" }],
+      incharge: [
+        { id: 51, name: "Dr. Pubali Mukherjee" },
+        { id: 52, name: "Dr. Tanmoy Roy Chowdhury" },
+      ],
       SubCommittee: [
-        { id: 12, name: "Mr. Sekhar Rana" },
-        { id: 13, name: "Mr. Sekhar Rana" },
-        { id: 14, name: "Mr. Sekhar Rana" },
-        { id: 15, name: "Mr. Sekhar Rana" },
-        { id: 16, name: "Mr. Sekhar Rana" },
+        { id: 53, name: "Dr. Brajendra Nath Dey" },
+        { id: 54, name: "Mr. Debanka De" },
       ],
       StudentMembers: [
-        { id: 17, name: "Ishika Sen ECE", phone: "6289649452", bio: "ECE" },
-        { id: 18, name: "Shailee Sharma", phone: "9123709838", bio: "ECE" },
+        { id: 55, name: "Rit Prasad Dhar", phone: "9830037938", bio: "AUE" },
+        { id: 56, name: "Sayan Paul", phone: "7003813146", bio: "CSE" },
+        { id: 57, name: "Kriti Ganeriwal", phone: "9831798205", bio: "ECE" },
       ],
     },
   },
@@ -137,19 +149,20 @@ const eventDetails = [
     amtout: "₹200",
     size: "4 members",
     googlr_Form: "https://forms.gle/N8Yp4VkrRMZyGjpT6",
-    rules: "https://meet.google.com/zua-gmya-vyf",
+    rules:
+      "https://drive.google.com/file/d/1pxwoWbMbcVzUPAg9WcT1PYQ12fHCTh8l/view?usp=share_link",
     contacts: {
-      incharge: [{ id: 11, name: "Ms. Sushmita Dey" }],
+      incharge: [
+        { id: 61, name: "Dr. Debopama Kar Ray (Basak)" },
+        { id: 62, name: "Mr Sudeep Samanta" },
+      ],
       SubCommittee: [
-        { id: 12, name: "Mr. Sekhar Rana" },
-        { id: 13, name: "Mr. Sekhar Rana" },
-        { id: 14, name: "Mr. Sekhar Rana" },
-        { id: 15, name: "Mr. Sekhar Rana" },
-        { id: 16, name: "Mr. Sekhar Rana" },
+        { id: 63, name: "Dr. Atanu Banerjee" },
+        { id: 64, name: "Dr. Suchismita Ghosh Mukherjee" },
       ],
       StudentMembers: [
-        { id: 17, name: "Ishika Sen ECE", phone: "6289649452", bio: "ECE" },
-        { id: 18, name: "Shailee Sharma", phone: "9123709838", bio: "ECE" },
+        { id: 65, name: "Arghya Pal", phone: "6289058933", bio: "EE" },
+        { id: 66, name: "Akansha Tripathi", phone: "6290839047", bio: "IT" },
       ],
     },
   },
@@ -161,19 +174,18 @@ const eventDetails = [
     amtout: "₹200 ",
     size: "3 members",
     googlr_Form: "https://forms.gle/v2yqUHwkHzQmNhdn6",
-    rules: "https://meet.google.com/zua-gmya-vyf",
+    rules:
+      "https://drive.google.com/file/d/1Qv_HddwMhaf9j0SDxYpUtzNKSjBl0KDf/view?usp=share_link",
     contacts: {
-      incharge: [{ id: 11, name: "Ms. Sushmita Dey" }],
+      incharge: [{ id: 71, name: "Dr. Shampa Sengupta" }],
       SubCommittee: [
-        { id: 12, name: "Mr. Sekhar Rana" },
-        { id: 13, name: "Mr. Sekhar Rana" },
-        { id: 14, name: "Mr. Sekhar Rana" },
-        { id: 15, name: "Mr. Sekhar Rana" },
-        { id: 16, name: "Mr. Sekhar Rana" },
+        { id: 72, name: "Dr. Joydeep Banerjee " },
+        { id: 73, name: "Mr. Priyanath Mahanti" },
       ],
       StudentMembers: [
-        { id: 17, name: "Ishika Sen ECE", phone: "6289649452", bio: "ECE" },
-        { id: 18, name: "Shailee Sharma", phone: "9123709838", bio: "ECE" },
+        { id: 74, name: "Sunetra Majumder", phone: "6289984395", bio: "ECE" },
+        { id: 75, name: "Tiasa Deshmukh ", phone: "9007049807", bio: "ECE" },
+        { id: 76, name: "Rit Prasad Dhar ", phone: "9830037938", bio: "AUE" },
       ],
     },
   },
@@ -185,9 +197,13 @@ const eventDetails = [
     amtout: "₹100",
     size: "2 members",
     googlr_Form: "https://forms.gle/14k7Hzp8UC1UCJ839",
-    rules: "https://meet.google.com/zua-gmya-vyf",
+    rules:
+      "https://drive.google.com/file/d/1KZFZfcbUT942b34b9vuP198mtI1NCQOO/view?usp=share_link",
     contacts: {
-      incharge: [{ id: 41, name: "Mr. Milan Chakraborty," },{ id: 42, name: "Mr. Sourav Ghorai" }],
+      incharge: [
+        { id: 41, name: "Mr. Milan Chakraborty," },
+        { id: 42, name: "Mr. Sourav Ghorai" },
+      ],
 
       SubCommittee: [{ id: 43, name: " Dr. Abhijit Pramanik" }],
       StudentMembers: [
@@ -204,19 +220,17 @@ const eventDetails = [
     amt: "Free",
     size: "3 members",
     googlr_Form: "https://forms.gle/14k7Hzp8UC1UCJ839",
-    rules: "https://meet.google.com/zua-gmya-vyf",
+    rules:
+      "https://drive.google.com/file/d/1s_aAPUBuSxXSOJ1z93xQWcZqe1WsKGie/view?usp=share_link",
     contacts: {
-      incharge: [{ id: 11, name: "Ms. Sushmita Dey" }],
+      incharge: [{ id: 91, name: "Dr. Juin Ghosh Sarkar" }],
       SubCommittee: [
-        { id: 12, name: "Mr. Sekhar Rana" },
-        { id: 13, name: "Mr. Sekhar Rana" },
-        { id: 14, name: "Mr. Sekhar Rana" },
-        { id: 15, name: "Mr. Sekhar Rana" },
-        { id: 16, name: "Mr. Sekhar Rana" },
+        { id: 92, name: "Mr. Sumit Majumdar" },
+        { id: 93, name: "Ms. Sulagna Chakraborty" },
       ],
       StudentMembers: [
-        { id: 17, name: "Ishika Sen ECE", phone: "6289649452", bio: "ECE" },
-        { id: 18, name: "Shailee Sharma", phone: "9123709838", bio: "ECE" },
+        { id: 94, name: "Rohit Samanta", phone: "7044719825", bio: "IT" },
+        { id: 95, name: "Rikita Koley", phone: "8697558062", bio: "IT" },
       ],
     },
   },
@@ -226,21 +240,28 @@ const eventDetails = [
     date: "5th January 2023",
     amtin: "₹50",
     amtout: "₹50",
-    size: "1 to 3",
+    size: "3 members",
     googlr_Form: "https://forms.gle/14k7Hzp8UC1UCJ839",
-    rules: "https://meet.google.com/zua-gmya-vyf",
+    rules:
+      "https://drive.google.com/file/d/1L34GxcA7Ch84c6-fOTHz1TZOI05OLFVd/view?usp=share_link",
     contacts: {
-      incharge: [{ id: 11, name: "Ms. Sushmita Dey" }],
+      incharge: [
+        { id: 101, name: "Mr. Milan Chakraborty" },
+        { id: 102, name: "Ms. Debanjali Chowdhury" },
+        { id: 103, name: "Mr. Santanu Maity" },
+        { id: 104, name: "Ms. Nabamita Majumder" },
+      ],
       SubCommittee: [
-        { id: 12, name: "Mr. Sekhar Rana" },
-        { id: 13, name: "Mr. Sekhar Rana" },
-        { id: 14, name: "Mr. Sekhar Rana" },
-        { id: 15, name: "Mr. Sekhar Rana" },
-        { id: 16, name: "Mr. Sekhar Rana" },
+        { id: 105, name: "Ms. Sasmita S Choudhury" },
+        { id: 106, name: "Mr. Kailash Chandra Mahatab" },
+        { id: 107, name: "Ms. Aditi Bandyopadhyay" },
+        { id: 108, name: "Mr. Mangal Singh" },
       ],
       StudentMembers: [
-        { id: 17, name: "Ishika Sen ECE", phone: "6289649452", bio: "ECE" },
-        { id: 18, name: "Shailee Sharma", phone: "9123709838", bio: "ECE" },
+        { id: 109, name: "Tiasa Deshmukh", phone: "9007049807", bio: "ECE" },
+        { id: 110, name: "Debarshi Roy", phone: "9002189996", bio: "IT" },
+        { id: 110, name: "Kriti Ganeriwal", phone: "9831798205", bio: "ECE" },
+        { id: 110, name: "Ishika Sen", phone: "6289649452", bio: "ECE" },
       ],
     },
   },
@@ -252,9 +273,13 @@ const eventDetails = [
     amtout: "₹100",
     size: "1 member",
     googlr_Form: "https://forms.gle/14k7Hzp8UC1UCJ839",
-    rules: "https://meet.google.com/zua-gmya-vyf",
+    rules:
+      "https://drive.google.com/file/d/1YOd9Tkl3pyqLRqGEdkP5oqNQU_-SAy5h/view?usp=share_link",
     contacts: {
-      incharge: [{ id: 111, name: "Ms Jayanti Mahata" },{ id: 112, name: "Mr. Basudev Ghatak" }],
+      incharge: [
+        { id: 111, name: "Ms Jayanti Mahata" },
+        { id: 112, name: "Mr. Basudev Ghatak" },
+      ],
 
       SubCommittee: [{ id: 113, name: " Mr. Amlan Karmakar" }],
 
@@ -273,9 +298,13 @@ const eventDetails = [
     amtout: "₹100",
     size: "1 member",
     googlr_Form: "https://forms.gle/14k7Hzp8UC1UCJ839",
-    rules: "https://meet.google.com/zua-gmya-vyf",
+    rules:
+      "https://drive.google.com/file/d/19qjROzzuFbOkUChABWnzlPenQYzl3mMP/view?usp=share_link",
     contacts: {
-      incharge: [{ id: 121, name: "Mr. Santanu Maity" },{ id: 122, name: "Mr. Basudev Ghatak" }],
+      incharge: [
+        { id: 121, name: "Mr. Santanu Maity" },
+        { id: 122, name: "Mr. Basudev Ghatak" },
+      ],
 
       SubCommittee: [{ id: 123, name: " Mr. Sandip Chakraborty" }],
 
@@ -304,9 +333,14 @@ const eventDetails = [
     amtout: "₹400",
     size: "2 to 4",
     googlr_Form: "https://forms.gle/14k7Hzp8UC1UCJ839",
-    rules: "https://meet.google.com/zua-gmya-vyf",
+    rules:
+      "https://drive.google.com/file/d/1Z5SnKAF-bdSSDMSgsBjt25SVVIC0tQKS/view?usp=share_link",
     contacts: {
-      incharge: [{ id: 131, name: "Dr. Abhijit Dutta" },{ id: 132, name: "Mr. Tapas Kumar Biswas" },{ id: 133, name: "Mr. Nabankur Mandal" }],
+      incharge: [
+        { id: 131, name: "Dr. Abhijit Dutta" },
+        { id: 132, name: "Mr. Tapas Kumar Biswas" },
+        { id: 133, name: "Mr. Nabankur Mandal" },
+      ],
       SubCommittee: [
         { id: 134, name: " Mr. Bimal Makhal" },
         { id: 135, name: "Mr. Drupad Roy" },
@@ -333,9 +367,14 @@ const eventDetails = [
     amtout: "₹400",
     size: "2 to 4 members",
     googlr_Form: "https://forms.gle/14k7Hzp8UC1UCJ839",
-    rules: "https://meet.google.com/zua-gmya-vyf",
+    rules:
+      "https://drive.google.com/file/d/14yW_SZ948crKGSAGNyfpdokdmMgGOYCb/view?usp=share_link",
     contacts: {
-      incharge: [{ id: 141, name: "Dr. Abhijit Dutta" },{ id: 142, name: "Mr. Tapas Kumar Biswas" },{ id: 143, name: "Mr. Nabankur Mandal" }],
+      incharge: [
+        { id: 141, name: "Dr. Abhijit Dutta" },
+        { id: 142, name: "Mr. Tapas Kumar Biswas" },
+        { id: 143, name: "Mr. Nabankur Mandal" },
+      ],
       SubCommittee: [
         { id: 144, name: " Mr. Bimal Makhal" },
         { id: 145, name: "Mr. Drupad Roy" },
@@ -367,10 +406,15 @@ const eventDetails = [
     amtout: "₹400",
     size: "2 members",
     googlr_Form: "https://forms.gle/14k7Hzp8UC1UCJ839",
-    rules: "https://meet.google.com/zua-gmya-vyf",
+    rules:
+      "https://drive.google.com/file/d/1OWII5jyDFKt3dySlE0BUcb-LcTiDny43/view?usp=share_link",
 
     contacts: {
-      incharge: [{ id: 141, name: "Dr. Abhijit Dutta" },{ id: 142, name: "Mr. Tapas Kumar Biswas" },{ id: 143, name: "Mr. Nabankur Mandal" }],
+      incharge: [
+        { id: 141, name: "Dr. Abhijit Dutta" },
+        { id: 142, name: "Mr. Tapas Kumar Biswas" },
+        { id: 143, name: "Mr. Nabankur Mandal" },
+      ],
 
       SubCommittee: [
         { id: 154, name: " Mr. Bimal Makhal" },
@@ -402,10 +446,55 @@ const eventDetails = [
     amtout: "₹400",
     size: "2 to 4 members",
     googlr_Form: "https://forms.gle/14k7Hzp8UC1UCJ839",
+    rules:
+      "https://drive.google.com/file/d/1e-NuYNxDU3yJWW0ez5ma_qA29rXaVIX1/view?usp=share_link",
+
+    contacts: {
+      incharge: [
+        { id: 141, name: "Dr. Abhijit Dutta" },
+        { id: 142, name: "Mr. Tapas Kumar Biswas" },
+        { id: 143, name: "Mr. Nabankur Mandal" },
+      ],
+
+      SubCommittee: [
+        { id: 164, name: " Mr. Bimal Makhal" },
+        { id: 165, name: "Mr. Drupad Roy" },
+        { id: 166, name: "Mr. Dhiman Bhattacharjee" },
+        { id: 167, name: "Mr. Rajib Majhi" },
+        { id: 168, name: "Mr. Samrat Purkait " },
+        { id: 169, name: "Mr. Om Prakash" },
+        { id: 1610, name: "Mr. Arijit Ghosh" },
+        { id: 1611, name: "Mr. Joydeb Karmakar" },
+        { id: 1612, name: "Mr. Ashis Taru Roy" },
+      ],
+      StudentMembers: [
+        { id: 1613, name: "Ayan Palit", phone: "6295389311", bio: "EE" },
+        {
+          id: 1614,
+          name: " Nageswar Rao Enduga",
+          phone: "8420559756",
+          bio: "ME",
+        },
+        { id: 1615, name: " Abhishek Kumar", phone: "6207902850", bio: "EE" },
+      ],
+    },
+  },
+  {
+    id: 17,
+    name: "Line Follower",
+    date: "5th January 2023",
+    amtin: "₹100",
+    amtout: "₹400",
+    size: "2 to 4 members",
+    googlr_Form: "https://forms.gle/14k7Hzp8UC1UCJ839",
     rules: "https://meet.google.com/zua-gmya-vyf",
 
     contacts: {
-      incharge: [{ id: 141, name: "Dr. Abhijit Dutta" },{ id: 142, name: "Mr. Tapas Kumar Biswas" },{ id: 143, name: "Mr. Nabankur Mandal" }],
+      incharge: [
+        { id: 141, name: "Dr. Abhijit Dutta" },
+        { id: 142, name: "Mr. Tapas Kumar Biswas" },
+        { id: 143, name: "Mr. Nabankur Mandal" },
+      ],
 
       SubCommittee: [
         { id: 164, name: " Mr. Bimal Makhal" },

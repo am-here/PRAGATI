7e7a88dc65d0231913dbd8448df816ecb82629ef
am-here <harshbaid.ind@gmail.com>
2022-12-17 14:34:22 +0530

    contacts added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,6 +4,7 @@ import HeroSection from "../components/HeroSection/HeroSection";
 import Venue from "../components/Venue";
 import EventCard from "../components/Ongoing_Event/EventCard";
 import Event_Detail from "../components/Event_Details/event_details";
+import Event_Contact from "../components/Event_Details/event_contacts";
 
 function Home() {
   return (
@@ -11,6 +12,7 @@ function Home() {
       <HeroSection />
       <EventCard />
       <Event_Detail name="Circuit Design" date="5th January 2023" amt="50" size="1 to 3" rules="https://meet.google.com/zua-gmya-vyf" contacts="https://meet.google.com/zua-gmya-vyf"/>
+      <Event_Contact/>
       {/* <EventCard /> */}
       <Venue />
     </>

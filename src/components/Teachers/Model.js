e8b0a53118a48d67b882f3e237da9586c83f884b
Ishika Ghosh <idofishika321@gmail.com>
2022-12-26 20:48:03 +0530

    contact page responsive

--- src/components/Teachers/Model.js
+++ src/components/Teachers/Model.js
@@ -3,40 +3,45 @@ const contact_data = [
     id: 1,
     name: "Ishika Ghosh",
     bio: "Developer",
-    phone: 8910631623,
+    phone: 6290399626,
     linkedin: "https://www.linkedin.com/in/ishika-ghosh20112002/",
-    image: "https://media.licdn.com/dms/image/D5635AQHFVkpm_WmDHQ/profile-framedphoto-shrink_800_800/0/1668695287074?e=1672642800&v=beta&t=sfo6eQ8UVKxw2mVo4oinOtPZVFq04LDj4SNlk0cInpQ",
+    image:
+      "https://media.licdn.com/dms/image/D5635AQHFVkpm_WmDHQ/profile-framedphoto-shrink_800_800/0/1668695287074?e=1672642800&v=beta&t=sfo6eQ8UVKxw2mVo4oinOtPZVFq04LDj4SNlk0cInpQ",
   },
   {
     id: 2,
     name: "Utkarsh Kr Singh",
     bio: "Developer",
     linkedin: "https://www.linkedin.com/in/utkarsh-kumar-singh-91b1a2205/",
-    image: "https://media.licdn.com/dms/image/D5603AQFfJmQ_lcGiLw/profile-displayphoto-shrink_400_400/0/1668356492230?e=1677715200&v=beta&t=x708_fF4XhimuUkS9buUNp9tbCjuDGD_hRbsrjrl47w",
-    phone: 8910631623,
+    image:
+      "https://media.licdn.com/dms/image/D5603AQFfJmQ_lcGiLw/profile-displayphoto-shrink_400_400/0/1668356492230?e=1677715200&v=beta&t=x708_fF4XhimuUkS9buUNp9tbCjuDGD_hRbsrjrl47w",
+    phone: 7370988146,
   },
   {
     id: 3,
     name: "Nitu Kumari",
     bio: "Developer",
     linkedin: "https://www.linkedin.com/in/nitu-kumari-46a8541bb/",
-    image: "https://media.licdn.com/dms/image/C4E03AQH5isegAYh7XQ/profile-displayphoto-shrink_100_100/0/1605784349529?e=1677715200&v=beta&t=R6ue22ZHBLOeKBGwWI8Dmh90rh8sLfryoUgiPiBxt0A",
-    phone: 8910631623,
+    image:
+      "https://media.licdn.com/dms/image/C4E03AQH5isegAYh7XQ/profile-displayphoto-shrink_100_100/0/1605784349529?e=1677715200&v=beta&t=R6ue22ZHBLOeKBGwWI8Dmh90rh8sLfryoUgiPiBxt0A",
+    phone: 8585030031,
   },
   {
     id: 4,
     name: "Abhishek Mishra",
     bio: "Developer",
     linkedin: "https://www.linkedin.com/in/abhishek-mishra-b4a275202/",
-    image: "https://media.licdn.com/dms/image/D4D03AQEKtZjoBbfvrA/profile-displayphoto-shrink_100_100/0/1670940726259?e=1677715200&v=beta&t=wBS5VF54wO1_XJv5ZTPQxH9IaJ9VZaD60s0aDGynUEg",
-    phone: 8910631623,
+    image:
+      "https://media.licdn.com/dms/image/D4D03AQEKtZjoBbfvrA/profile-displayphoto-shrink_100_100/0/1670940726259?e=1677715200&v=beta&t=wBS5VF54wO1_XJv5ZTPQxH9IaJ9VZaD60s0aDGynUEg",
+    phone: 8887850034,
   },
   {
     id: 5,
     name: "Harsh Baid",
     bio: "Developer",
     linkedin: "https://www.linkedin.com/in/harsh-baid-b664b81b8/",
-    image: "https://media.licdn.com/dms/image/D4D03AQFZda_-8PqCOw/profile-displayphoto-shrink_400_400/0/1672039935393?e=1677715200&v=beta&t=e8Og07vb3pATG3d_bed169lPxGIw2xX48r-jovw2awc",
+    image:
+      "https://media.licdn.com/dms/image/D4D03AQFZda_-8PqCOw/profile-displayphoto-shrink_400_400/0/1672039935393?e=1677715200&v=beta&t=e8Og07vb3pATG3d_bed169lPxGIw2xX48r-jovw2awc",
     phone: 8910631623,
   },
 ];
